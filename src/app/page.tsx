2da765ff5f41bf7fd59481056656ef856ab1d65f
Ovidiu Militaru <ovidiu.militaru@whitbread.com>
2024-01-11 11:01:52 +0200

    added HP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 'use client';
 
+import HomeScreenComponent from '@/components/HomeScreen/HomeScreen.component';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import styles from './page.module.css'
 
 export default function Home() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <main className={styles.main}>
-        <p> WIP </p>
-      </main>
+      <HomeScreenComponent />
     </QueryClientProvider>
   )
 }
--- src/utils/getters/getRandomUser.ts
+++ src/utils/getters/getRandomUser.ts
@@ -1,6 +1,5 @@
 // This function generate a random integer number between 1 and 10
 
-export function getRandomUser(): number {
-
-  return Math.ceil(Math.random() * 10)
+export function getRandomUser(): string {
+  return Math.ceil(Math.random() * 10).toString()
 }
