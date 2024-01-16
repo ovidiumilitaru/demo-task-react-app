c47775e69dd368f3fd243226253b8e4887dc10ee
Ovidiu Militaru <ovidiu.militaru@whitbread.com>
2024-01-16 11:01:31 +0200

    edit post done

--- src/utils/queries/deletePost.ts
+++ src/utils/queries/deletePost.ts
@@ -1,20 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { BASE_URL, POSTS_URL_PART, METHOD } from "@/utils/constants/constants";
 
 export const deletePostMutation = (postId: string) => {
-  
+  const queryClient = useQueryClient();
   const url = `${BASE_URL}${POSTS_URL_PART}/${postId}`;
-  
   const method = METHOD.DELETE;
+
   const fetchDeletePost = async () => {
-    const res = await fetch(url, { method });
-    return res
+    const result = await fetch(url, { method });
+    return result;
   }
 
   return useMutation({
     mutationFn: fetchDeletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['getPost']});
+      queryClient.invalidateQueries({ queryKey: ['getUserPosts']});
+    }
   });
 }
-
-
-
